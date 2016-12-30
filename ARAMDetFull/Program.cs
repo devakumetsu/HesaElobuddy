c86b1474f692a849c1020a755fe1d727d92387bf
scarEl <inspirationgamer@yahoo.de>
2016-12-30 17:05:10 +0100

    Auto Attacks

--- ARAMDetFull/Program.cs
+++ ARAMDetFull/Program.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Linq;
 using EloBuddy;
 using EloBuddy.SDK;
+using static ARAMDetFull.ARAMDetFull;
 
 namespace ARAMDetFull
 {
@@ -12,33 +13,29 @@ namespace ARAMDetFull
 
         public static void Main(string[] args)
         {
-            new ARAMDetFull();
             Game.OnTick += OnTick;
-            Drawing.OnDraw += OnDraw;
+            new ARAMDetFull();
+            
         }
 
         private static void OnTick(EventArgs args)
         {
-            var target = ARAMTargetSelector.getBestTarget(550, true);
-            //var target = TargetSelector.GetTarget(Player.Instance.GetAutoAttackRange(), DamageType.Mixed,Player.Instance.Position);
-            var bTarg = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, Player.Instance.ServerPosition, Player.Instance.GetAutoAttackRange()).OrderBy(x => x.Distance(Player.Instance)).FirstOrDefault();
-            if (Player.Instance.CountEnemiesInRange(550) <= 1 && Player.Instance.CountAlliesInRange(550) <=1)
+            var pig = Player.Instance.GetAutoAttackRange();
+            var target = ARAMTargetSelector.getBestTarget(Player.Instance.GetAutoAttackRange(),true);
+            //var target = TargetSelector.GetTarget(Player.Instance.GetAutoAttackRange(),DamageType.Physical,Player.Instance.Position);
+            var bTarg = EntityManager.MinionsAndMonsters.GetLaneMinions(EntityManager.UnitTeam.Enemy, Player.Instance.Position, Player.Instance.GetAutoAttackRange()).OrderBy(x => x.Distance(Player.Instance)).FirstOrDefault();
+            if (Player.Instance.IsInRange(target,pig) && Player.Instance.CountEnemiesInRange(550) <= 1 && Player.Instance.CountAlliesInRange(550) <=1)
             {
                 Player.IssueOrder(GameObjectOrder.AttackUnit, target);
             }
+
             else
             {
-                if (Player.Instance.CountEnemiesInRange(1000) == 0 && Player.Instance.CountAlliesInRange(1000) <= 1)
+                if (Player.Instance.CountEnemiesInRange(500) <= 0 && Player.Instance.CountEnemyMinionsInRange(500) <=1)
                 {
                     Player.IssueOrder(GameObjectOrder.AttackUnit, bTarg);
                 }
             }
-        }
-
-        private static void OnDraw(EventArgs args)
-        {
-            Drawing.DrawCircle(Player.Instance.Position, 1000, Color.Red);
-            Drawing.DrawCircle(Player.Instance.Position, 550, Color.White);
         }
     }
 }
