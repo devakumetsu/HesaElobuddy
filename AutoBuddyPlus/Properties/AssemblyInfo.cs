5aab425a5287332baaa3f06075332716311e8e28
Akumetsu <otaku.akumetsu@gmail.com>
2017-07-10 15:27:37 -0400

    set version.
    set the version to the last big update date (yeah.month.version)

--- AutoBuddyPlus/Properties/AssemblyInfo.cs
+++ AutoBuddyPlus/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AutoBuddyPlus")]
 [assembly: AssemblyDescription("Created by Christian Brutal Sniper and updated by Hesa, Tryller And DevAkumetsu")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hesa")]
+[assembly: AssemblyCompany("Brutal")]
 [assembly: AssemblyProduct("AutoBuddyPlus")]
 [assembly: AssemblyCopyright("Copyright Christian Brutal Sniper@Elobuddy  2015")]
 [assembly: AssemblyTrademark("")]
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("2017.7.1")]
+[assembly: AssemblyFileVersion("2017.7.1")]
